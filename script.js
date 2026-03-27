86c3a1df2cfc29339f6101ad337baa3f3b0ffd51
Md Taufique Alam <taufiquealam2404@gmail.com>
2026-03-27 17:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get current URL
+const url = window.location.href;
+
+// Get length of URL
+const length = url.length;
+
+// Show result using alert
+alert("The length of the URL is: " + length);
